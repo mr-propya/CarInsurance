8db53f9e6e1601cd4e859cfcdb1aadeec5399de5
Abhishek <abhirevadekar12@gmail.com>
2019-09-04 20:00:58 +0530

    classes created

--- carInsurance/androidApis/models.py
+++ carInsurance/androidApis/models.py
@@ -18,3 +18,35 @@ class Car(models.Model):
     vehicleNum = models.CharField(max_length=20)
     purchasedAt = models.DateTimeField(blank=True)
     model = models.CharField(max_length=50)
+    owner = models.ForeignKey(Customer,on_delete=models.CASCADE())
+
+
+class Insurance(models.Model):
+    dateOfInsurance = models.DateField()
+    monthlyPremium = models.FloatField()
+    balance = models.FloatField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE())
+
+
+class Claims(models.Model):
+    verified = models.BooleanField()
+    raisedAt = models.DateTimeField()
+    settledAt = models.DateTimeField()
+    price = models.IntegerField()
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE())
+
+
+class Predictions(models.Model):
+    predictedAt = models.DateTimeField()
+    predictedPrice = models.IntegerField()
+    actualPrice = models.IntegerField()
+    claim = models.ForeignKey(Claims, on_delete=models.CASCADE())
+
+
+class Transactions(models.Model):
+    time = models.TimeField()
+    transactionId = models.IntegerField()
+    status = models.CharField(max_length=20)
+    car = models.ForeignKey(Car,on_delete=models.CASCADE())
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE())
+
